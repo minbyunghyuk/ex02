3bf03752ca90cb2d62568e078416c82e7462d9fa
minbyunghyuk <ptsdkr@naver.com>
2020-03-21 17:49:24 +0900

    log.xml change

--- src/main/java/org/zerock/domain/SampleVO.java
+++ src/main/java/org/zerock/domain/SampleVO.java
@@ -3,10 +3,6 @@ package org.zerock.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
-
-
 @Data
 @NoArgsConstructor //비어있는생성자 
 @AllArgsConstructor // 모든속성을 사용하는 생성자 
